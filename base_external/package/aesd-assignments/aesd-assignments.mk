24facc957bd1137932b22f6af36181ba9e76bf8b
Adilson Dias <295315500+adilsonsdias-msece@users.noreply.github.com>
2026-06-21 14:19:55 +1000

    assigment completed

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,13 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 7bce9be84f161c979d21da84d7444c8ce4b3542b '#COMMIT VERSION NUMBER'
+AESD_ASSIGNMENTS_VERSION = 7bce9be84f161c979d21da84d7444c8ce4b3542b 
+#COMMIT VERSION NUMBER'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-adilsonsdias-msece.git '#GITHUB REPOSITORY LINK'
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-adilsonsdias-msece.git
+#GITHUB REPOSITORY LINK'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
